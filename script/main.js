8fdc94e88e9edf94b1030103db6cbdd787cabddf
hpteach2021 <hooper.rob@gmail.com>
2021-09-21 20:38:11 +0100

    trimmed down some more but need to restructure functions to be pure

--- script/main.js
+++ script/main.js
@@ -7,18 +7,12 @@ const buttonFromMorse = document.querySelector('.translator__buttonMorse');
 const buttonReset = document.querySelector('.btn-reset');
 const buttonPlay = document.querySelector('.btn-play');
 
-
-
-
 //event listeners
 
 buttonFromEnglish.addEventListener(('click'), () => translateToMorse(boxEnglish));
-// buttonFromEnglish.addEventListener(('click'), () => colors());
 buttonFromMorse.addEventListener(('click'), () => translateToEnglish(boxMorse));
 buttonReset.addEventListener(('click'), () => reset());
 buttonPlay.addEventListener(('click'), () => play());
-
-
 
 //Function - English to Mors
 
@@ -28,72 +22,48 @@ if (format.test(string.value)) {
   string.style.color = "#ff0000";
   string.style.fontWeight = "900";
   string.value  = incorrectInput
-
   return incorrectInput;
-  
-  
 }
 else {
 const morseArray = []
   const stringAsArray = Array.from(string.value.toLowerCase());
-
   stringAsArray.forEach(letter => { 
     const letterValue = englishAlphabet.indexOf(letter);
     morseArray.push(morseAlphabet[letterValue])
   });
-  
   const arrayAsString = morseArray.join(" ")
   boxMorse.value = arrayAsString;
   return arrayAsString;
-  
-
 }
 }
-
-
 //Function - Morse to English 
 const translateToEnglish = (string) => {
 let validInput = /•—/i
-
 if (validInput.test(string.value)) {
   const englishArray = []
   const morseAsArray =  string.value.split(" ");
   morseAsArray.forEach(character => {
     const charValue = morseAlphabet.indexOf(character);
     englishArray.push(englishAlphabet[charValue])
-    
-  
 });
 
 const arrayAsString = englishArray.join("");
 boxEnglish.value = arrayAsString;
 return arrayAsString;
-
 }
 else {
   const incorrectInput = "Not a valid morse code character!"
   string.style.color = "#ff0000";
   string.style.fontWeight = "900";
-  string.value  = incorrectInput
+  string.value = incorrectInput
   return incorrectInput;
-
 }
-
-    
-
-
-
-
-
 };
 
 //Function to Play Sound
 const play = () => {
   let morse = new morseSynth();
   morse.play(boxEnglish.value);
-
-  // boxEnglish.value = "";
-  // boxMorse.value = "";
 }
 
 const reset = () => {
@@ -103,13 +73,4 @@ const reset = () => {
   boxMorse.style.fontWeight = "normal";
   boxEnglish.style.color = "#302B27";
   boxEnglish.style.fontWeight = "normal";
-}
-
-
-const colors = () => {
-  if (format.test(boxEnglish.value)) {
-    boxEnglish.style.color = "#ff0000";
-    boxEnglish.style.fontWeight = "900";
-
-}
 }
